5da06484bc7815afcfa4ad287286054f8aec7614
elena-miles <elena.airey@gmail.com>
2025-06-18 11:13:09 +0200

    updates

--- src/App.js
+++ src/App.js
@@ -4,10 +4,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      <h1>Hi </h1>
-        <img src={logo} className="App-logo" alt="logo" />
+      <header className="App-Logo">
+        <a href="https://www.shecodes.io" target="_blank" rel="noopener noreferrer">
+          <img className="App-logo" alt="SheCodeslogo" src="https://www.weather.shecodes.io/images/logo.png"/>
+        </a>
       </header>
+      <body>
+      </body>
+      <footer>
+      <p class="footer-message"> This project was coded by <strong>Elena Miles</strong> and is open sourced on <a href="https://github.com/elena-miles/weatherreact3" target="_blank">github</a> and hosted with <a href="https://emweatherreact3.netlify.app/">netlify</a>.</p>
+      </footer>
     </div>
   );
 }
