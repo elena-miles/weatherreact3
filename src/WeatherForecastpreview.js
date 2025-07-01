87baec144a91d1c7846498d071af20ad52f0816a
elena-miles <elena.airey@gmail.com>
2025-07-01 11:39:55 +0200

    Update WeatherForecastpreview.js

--- src/WeatherForecastpreview.js
+++ src/WeatherForecastpreview.js
@@ -1,23 +1,21 @@
 import React from "react";
 
 export default function WeatherForecastpreview(props) {
-
-        function day() {
+  if (!props.data) {
+    return null;
+  }
+function day() {
           let date = new Date(props.data.time * 1000);
           let day = date.getDay();
           let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
           return days[day];
-        }
-      
-        function maxTemperature() {
+        }    
+function maxTemperature() {
           let temperature = Math.round(props.data.temperature.maximum);
-      
           return `${temperature}°`;
         }
-      
-        function minTemperature() {
+function minTemperature() {
           let temperature = Math.round(props.data.temperature.minimum);
-      
           return `${temperature}°`;
         }
 
